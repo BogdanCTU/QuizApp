cdc5af4126b2d80914cad56b9a2059b3b864736f
BogdanCT <b.c.todoran@gmail.com>
2022-01-19 19:18:18 +0200

    Added User

---
+++ User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Laborator5_proj
+{
+    public class User
+    {
+        [Key]
+        public int IdUser { get; set; }
+        [MaxLength(30, ErrorMessage = "User's name max lenght is 30")]
+        public string UserName { get; set; }
+        [MaxLength(30, ErrorMessage = "User's surname max lenght is 30")]
+        public string UserSurname { get; set; }
+        [MaxLength(30, ErrorMessage = "User's username max lenght is 30")]
+        public string UserUsername { get; set; }
+        public string UserPassword { get; set; }
+    }
+
+    public class UserDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+
+    //EOF
+}
